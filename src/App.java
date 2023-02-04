cae1f89c0917df6f0fac7dcb379f2b4a52ee2db9
xzoom117 <amanwhohave@gmail.com>
2023-02-04 22:20:52 +0700

    Pertama opload

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+
+    /**
+     * The main function is a function that takes an array of strings and returns
+     * nothing.
+     * 
+     * @param args gak dipakai
+     * @throws Exception gak dipakai
+     */
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
